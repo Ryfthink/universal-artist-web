94938b1e5c3ac79e1d0490afda6fe7e5a27e2d48
RenYufeng <seven__up@sina.cn>
2018-06-07 23:11:27 +0800

    chore: qiniu img pipe

--- src/app/util/qiniu-img.pipe.ts
+++ src/app/util/qiniu-img.pipe.ts
@@ -1,7 +1,4 @@
 import {Pipe, PipeTransform} from '@angular/core';
-import {Util} from './util';
-
-const supportsWebP = Util.checkSupportWebp();
 
 @Pipe({
   name: 'qiniuImg'
@@ -10,17 +7,9 @@ export class QiniuImgPipe implements PipeTransform {
 
   transform(value: any, args?: any): any {
     if (args === 'normal') {
-      if (supportsWebP) {
-        return `${value}?imageView2/1/w/450/h/330/format/webp/q/75|imageslim`;
-      } else {
-        return `${value}?imageView2/1/w/450/h/330/q/75|imageslim`;
-      }
+      return `${value}?imageView2/1/w/450/h/330/q/75|imageslim`;
     } else if (args === 'large') {
-      if (supportsWebP) {
-        return `${value}?imageView2/1/w/450/h/800/format/webp/q/75|imageslim`;
-      } else {
-        return `${value}?imageView2/1/w/450/h/800/q/75|imageslim`;
-      }
+      return `${value}?imageView2/1/w/450/h/800/q/75|imageslim`;
     }
     return value;
   }
--- src/app/util/util.ts
+++ src/app/util/util.ts
@@ -1,12 +1,4 @@
 export class Util {
-  static checkSupportWebp(): boolean {
-    try {
-      return (document.createElement('canvas').toDataURL('image/webp').indexOf('data:image/webp') == 0);
-    } catch (err) {
-      return false;
-    }
-  }
-
   static getMailtoUrl(to, subject, body) {
     const args = [];
     if (typeof subject !== 'undefined') {
@@ -21,5 +13,19 @@ export class Util {
       url += '?' + args.join('&');
     }
     return url;
+  }
+
+  static async WebpIsSupported() {
+    // If the browser doesn't has the method createImageBitmap, you can't display webp format
+    if (!self.createImageBitmap) { return false; }
+
+    // Base64 representation of a white point image
+    const webpData = 'data:image/webp;base64,UklGRiQAAABXRUJQVlA4IBgAAAAwAQCdASoCAAEAAQAcJaQAA3AA/v3AgAA=';
+
+    // Retrieve the Image in Blob Format
+    const blob = await fetch(webpData).then(r => r.blob());
+
+    // If the createImageBitmap method succeeds, return true, otherwise false
+    return createImageBitmap(blob).then(() => true, () => false);
   }
 }
