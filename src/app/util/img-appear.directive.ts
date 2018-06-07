5d9d186fa9bec6ad2918086b63ab0e23d980acc2
Septenary <seven__up@sina.cn>
2018-06-07 20:56:12 +0800

    chore: imgAppearDirective init opcacity 0

--- src/app/util/img-appear.directive.ts
+++ src/app/util/img-appear.directive.ts
@@ -6,6 +6,7 @@ import {Directive, ElementRef, HostListener, Renderer2} from '@angular/core';
 export class ImgAppearDirective {
 
   constructor(private elementRef: ElementRef, private render: Renderer2) {
+    this.render.setStyle(this.elementRef.nativeElement, 'opacity', '0');
   }
 
   @HostListener('load')
