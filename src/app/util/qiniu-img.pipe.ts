2cb0f06b534998cfc272531e2b7e5826ceaa909f
RenYufeng <seven__up@sina.cn>
2018-09-17 23:03:00 +0800

    feat: 调整加载七牛图片预览尺寸

--- src/app/util/qiniu-img.pipe.ts
+++ src/app/util/qiniu-img.pipe.ts
@@ -7,9 +7,9 @@ export class QiniuImgPipe implements PipeTransform {
 
   transform(value: any, args?: any): any {
     if (args === 'normal') {
-      return `${value}?imageView2/1/w/450/h/330/q/75|imageslim`;
+      return `${value}?imageView2/1/w/540/h/500/q/100`;
     } else if (args === 'large') {
-      return `${value}?imageView2/1/w/450/h/800/q/75|imageslim`;
+      return `${value}?imageView2/1/w/540/h/960/q/100`;
     }
     return value;
   }
